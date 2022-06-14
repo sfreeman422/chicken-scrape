e2d7114f36c87103243536f172af55a0bfed14d3
steve <sfreeman422@protonmail.com>
2022-06-14 13:45:47 -0400

    Added DBUS_SESSSION_BUS_ADDRESS env var to the script

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
+PID=$(pgrep gnome-session | tail -n1)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 . /home/steve/.profile
 PATH=/usr/local/bin:$PATH
 cd /home/steve/code/chicken-scrape
